e6c13163a4a2c7a8b460a810004e349f97948369
PaulGrishunin <paul_gri@mail.ru>
2021-02-18 18:14:34 +0100

    add db

--- app.py
+++ app.py
@@ -9,6 +9,15 @@ from starlette.staticfiles import StaticFiles
 from starlette.config import Config
 from starlette.responses import JSONResponse
 
+from starlette.authentication import (
+    AuthenticationBackend, AuthenticationError, SimpleUser, UnauthenticatedUser,
+    AuthCredentials)
+from starlette.authentication import requires
+from starlette.middleware import Middleware
+from starlette.middleware.authentication import AuthenticationMiddleware
+import base64
+import binascii
+
 templates = Jinja2Templates(directory='templates')
 
 # Configuration from environment variables or '.env' file.
@@ -25,6 +34,25 @@ users = sqlalchemy.Table(
     sqlalchemy.Column("password", sqlalchemy.String),
 )
 database = databases.Database(DATABASE_URL)
+
+
+class BasicAuthBackend(AuthenticationBackend):
+    async def authenticate(self, request):
+        if "Authorization" not in request.headers:
+            return
+
+        auth = request.headers["Authorization"]
+        try:
+            scheme, credentials = auth.split()
+            if scheme.lower() != 'basic':
+                return
+            decoded = base64.b64decode(credentials).decode("ascii")
+        except (ValueError, UnicodeDecodeError, binascii.Error) as exc:
+            raise AuthenticationError('Invalid basic auth credentials')
+
+        username, _, password = decoded.partition(":")
+        # TODO: You'd want to verify the username and password here.
+        return AuthCredentials(["authenticated"]), SimpleUser(username)
 
 
 async def user_register(request):
@@ -46,11 +74,16 @@ async def auth(request):
     return templates.TemplateResponse('index.html', {'request': request})
     #return PlainTextResponse("Authorization")
 
+@requires('authenticated', redirect='auth')
 async def dashboard(request):
-    async def get(self, request):
-        username = request.path_params['username']
-        return PlainTextResponse(f"Привет, {username}")
+    #async def get(self, request):
+        #username = request.path_params['username']
+        #return PlainTextResponse(f"Привет, {username}")
     #return JSONResponse({'hello': 'world'})
+    if request.user.is_authenticated:
+        return PlainTextResponse('Привет, ' + request.user.display_name)
+    return PlainTextResponse('Hello, you')
+
 
 async def logout(request):
     return PlainTextResponse("logout")
@@ -61,10 +94,15 @@ routes = [
     Route("/register", endpoint=user_register, methods=["POST"]),
     Route("/dashboard", endpoint=dashboard),
     Route("/logout", endpoint=logout, methods=["GET", "POST"]),
-    Mount('/static', StaticFiles(directory='static'), name='static'),
+    Mount('/static', app=StaticFiles(directory='static', packages=['bootstrap4']), name="static"),
+  
 ]
 
-app = Starlette(debug=True, routes=routes)
+middleware = [
+    Middleware(AuthenticationMiddleware, backend=BasicAuthBackend())
+]
+
+app = Starlette(debug=True, routes=routes, middleware=middleware)
 
 
 #@app.route('/create-user', methods=['POST'])
@@ -75,3 +113,4 @@ app = Starlette(debug=True, routes=routes)
     #data = {'id': id, 'name': name}
     #result = {'status_code': '0', 'status_message' : 'Success', 'data': data}
     #return jsonify(result)
+    
